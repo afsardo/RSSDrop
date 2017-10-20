16c1de003cfd920d857589ac8f85c98b8552462b
afsardo <a.fsardo@gmail.com>
2017-10-20 01:21:36 +0100

    Make call to our api and display results

--- app/src/components/RSSFeed.vue
+++ app/src/components/RSSFeed.vue
@@ -1,47 +1,65 @@
-<template>
+<style>
+    .mb-1em {
+        margin-bottom: 1em;       
+    }
+</style>
 
+<template>
 <div class="container">
     <div class="row">
         <div class="col-md-4 offset-md-3 mb-1">
-            <input class="Search__Input form-control" style="display: inline-block;" placeholder="Enter RSS URL">
+            <input class="Search__Input form-control" v-model="url" placeholder="Enter RSS URL">
         </div>
         <div class="col-md-2">
             <button @click.prevent="search" class="Search__Button btn btn-light w-100">Search</button>
         </div>
     </div>
 
-    <hr>
+    <template v-if="items.length > 0">
+        <hr>
 
-    <div class="row">
+        <div class="row mb-1em" v-for="item in items">
 
-        <div class="col-md-3 text-center">
-            <img src="http://placehold.it/150x150" class="img-fluid img-thumbnail">
+            <div class="col-md-3 text-center">
+                <img src="http://placehold.it/150x150" class="img-fluid img-thumbnail">
+            </div>
+            <div class="col-md-9">
+                <a target="_blank" rel="noopener noreferrer" :href="item.link">
+                    <h2>{{ item.title }}</h2>
+                </a>
+                <p v-html="item.description"></p>
+            </div>
+
         </div>
-        <div class="col-md-9">
-            <a target="_blank" rel="noopener noreferrer" href="#">
-                <h2>An example feed title</h2>
-            </a>
-            <p>In facilisis feugiat ullamcorper. Maecenas sit amet nunc magna. Sed magna lorem, fringilla quis sapien in, fringilla scelerisque nisl. Nam aliquet aliquet finibus. Morbi eget nibh nec justo cursus accumsan. Vivamus venenatis magna at suscipit sodales. Nam sed elit urna. Sed porta tortor rutrum nisi mollis porttitor. Sed sit amet porta sem, et pharetra sapien. Maecenas suscipit ligula ac sodales aliquet. Aenean ac eros euismod, scelerisque elit sed, viverra velit.</p>
-        </div>
-
-    </div>
+    </template>
 </div>
-  
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'RSSFeed',
 
     data () {
         return {
-            
+            url: '',
+            items: [],
         }
     },
 
     methods: {
         search() {
+            this.items = [];
             this.disableSearch();
+
+            axios.get("http://localhost:3000/rssfeed?rss_url=" + this.url).then(response => {
+                this.items = response.data.data;
+                this.enableSearch();
+            }).catch(error => {
+                alert(error.response.data.message);
+                this.enableSearch();
+            });
         },
 
         disableSearch() {
@@ -51,6 +69,15 @@ export default {
             SearchInput.disabled = true;
             SearchButton.classList.add("disabled");
             SearchButton.disabled = true;
+        },
+
+        enableSearch() {
+            const SearchInput = document.querySelector(".Search__Input");
+            const SearchButton = document.querySelector(".Search__Button");
+            SearchInput.classList.remove("disabled");
+            SearchInput.disabled = false;
+            SearchButton.classList.remove("disabled");
+            SearchButton.disabled = false;
         }
     }
 }
